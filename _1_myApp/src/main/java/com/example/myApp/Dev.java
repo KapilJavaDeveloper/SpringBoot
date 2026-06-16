e441ab81d87a10bbd83adab4e383ba79faf04a4d
Kapil <kk7094948@gmail.com>
2026-06-16 09:43:34 +0530

    myApp

---
+++ _1_myApp/src/main/java/com/example/myApp/Dev.java
@@ -0,0 +1,13 @@
+package com.example.myApp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+
+    public void build(){
+
+        System.out.println("working on awesome project.") ;
+
+    }
+}
